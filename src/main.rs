6f752080ea6376a4c24eb5800302f0b64e185cc3
AsahiTokunaga <hihimamug14@gmail.com>
2025-03-09 12:55:26 +0000

    Init Commit

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    fs,
+    env,
+};
+use dotenv::dotenv;
+
+enum HttpResponce {
+    OK,
+    NotFound,
+}
+
+impl ToString for HttpResponce {
+    fn to_string(&self) -> String {
+        match self {
+            HttpResponce::OK => String::from("HTTP/1.1 200 OK"),
+            HttpResponce::NotFound => String::from("HTTP/1.1 404 NotFound"),
+        }
+    }
+}
+
+fn main() {
+    dotenv().ok();
+    let listener = TcpListener::bind(env::var("LISTENER").unwrap()).unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handler(stream);
+    }
+}
+
+fn handler(mut stream: TcpStream) {
+    let reader = BufReader::new(&stream);
+    let request_line = reader.lines().next().unwrap().unwrap();
+
+    println!("Request: {}", request_line);
+
+    let (status_line, contents) = if request_line == "GET / HTTP/1.1" {
+        (HttpResponce::OK.to_string(), fs::read_to_string("index.html").unwrap())
+    } else {
+        (HttpResponce::NotFound.to_string(), fs::read_to_string("not_found.html").unwrap())
+    };
+    let contents_length = contents.len();
+
+    let responce = format!("{}\r\nContent-Length: {}\r\n\r\n{}", status_line, contents_length, contents);
+
+    stream.write_all(responce.as_bytes()).unwrap();
+}
+
